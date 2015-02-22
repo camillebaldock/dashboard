df2499f84ec35b5ea02e7365b5f398b3f7c29934
Camille Baldock <camille@camillebaldock.co.uk>
2015-02-22 13:12:53 +0000

    Gemnasium

---
+++ jobs/gemnasium.rb
@@ -0,0 +1,22 @@
+require 'rest_client'
+
+SCHEDULER.every '10m' do
+  response = RestClient.get "https://X:#{ENV["GEMNASIUM_TOKEN"]}@api.gemnasium.com/v1/projects"
+  results = JSON.parse(response.to_str)
+  colours = results["owned"].select do |result|
+    result["monitored"]
+  end.map do |monitored|
+    monitored["color"]
+  end
+  reds = colours.select { |colour| colour == "red" }
+  if reds.count > 0
+    send_event('gemnasium', { current: reds.count, status: 'danger' })
+  else
+    yellows = colours.select { |colour| colour == "yellow" }
+    if yellows.count > 0
+      send_event('gemnasium', { current: yellows.count, status: 'warning' })
+    else
+      send_event('gemnasium', { status: 'ok' })
+    end
+  end
+end
