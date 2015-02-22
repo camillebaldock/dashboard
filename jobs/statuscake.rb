e1b6fd37c4b2597a02ccde2e46e66db5ed3d05fa
Camille Baldock <camille@camillebaldock.co.uk>
2015-02-22 16:08:09 +0000

    Arrangement

--- jobs/statuscake.rb
+++ jobs/statuscake.rb
@@ -3,19 +3,23 @@ require 'pp'
 
 username = ENV["STATUSCAKE_USERNAME"]
 key = ENV["STATUSCAKE_KEY"]
-testids = ['111111', '1111111', '111111', '1111111']
 
 SCHEDULER.every "#{ENV["UPDATE_FREQUENCY"]}" do
   items=[]
   is_down=0
   overall_status='ok'
-  testids.each do |testid|
-    response = Net::HTTP.get_response(URI('https://www.statuscake.com/API/Tests/Details/?TestID='+testid+'&Username='+username+'&API='+key))
+  response = Net::HTTP.get_response(URI('https://www.statuscake.com/API/Tests/?Username='+username+'&API='+key))
+  all_tests = JSON.parse(response.body)
+  test_ids = all_tests.map do |site_test|
+    site_test["TestID"]
+  end
+  test_ids.each do |testid|
+    response = Net::HTTP.get_response(URI("https://www.statuscake.com/API/Tests/Details/?TestID=#{testid}&Username=#{username}&API=#{key}"))
     website = JSON.parse(response.body)
-    items << { site: website['WebsiteName'], status: website['Status'], lasttest: website['LastTested'] }
     if website['Status']!='Up'
+      items << { site: website['WebsiteName'], status: website['Status'], lasttest: website['LastTested'] }
       overall_status='warning'
     end
   end
-  send_event('statuscake', { items:items,overall_status: overall_status })
+  send_event('statuscake', { items:items, overall_status: overall_status })
 end
