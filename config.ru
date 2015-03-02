fba7583c9a7deaaca70f5395667e7b4057a33ad7
Camille Baldock <camille@camillebaldock.co.uk>
2015-03-02 00:15:45 +0000

    Rescuetime and 1/2/3/4 dashboards

--- config.ru
+++ config.ru
@@ -1,17 +1,16 @@
-require "sinatra/cyclist"
 require 'dashing'
 require 'redis-objects'
 
-redis_uri = URI.parse(ENV["REDISTOGO_URL"])
-Redis.current = Redis.new(:host => redis_uri.host,
-                          :port => redis_uri.port,
-                          :password => redis_uri.password)
+#redis_uri = URI.parse(ENV["REDISTOGO_URL"])
+#Redis.current = Redis.new(:host => redis_uri.host,
+#                          :port => redis_uri.port,
+#                          :password => redis_uri.password)
 
-set :history, Redis::HashKey.new('dashing-hash')
+#set :history, Redis::HashKey.new('dashing-hash')
 
 configure do
   set :auth_token, ENV["AUTH_TOKEN"]
-  set :default_dashboard, '_cycle?duration=10'
+  set :default_dashboard, 'home'
 
   helpers do
     def protected!
@@ -24,7 +23,5 @@ end
 map Sinatra::Application.assets_prefix do
   run Sinatra::Application.sprockets
 end
-
-set :routes_to_cycle_through, [:tasks, :tech, :entertainment]
 
 run Sinatra::Application
--- jobs/todo.rb
+++ jobs/todo.rb
@@ -1,16 +1,14 @@
 SCHEDULER.every "1h" do
-  #Entertainment
-  send_event('films', { current: 0 })
-  send_event('tv', { current: 0 })
-  send_event('podcasts', { current: 0 })
-  send_event('videos', { current: 0 })
-  send_event('pocket', { current: 0 })
-  send_event('books', { current: 0 })
-  send_event('rss', { current: 0 })
-  #Tasks
-  send_event('newsletter', { current: 1000 })
-  send_event('camille', photos: ["2015.jpg", "2013.png", "cookie.jpg", "2014.jpg"])
-  #Tech
-  send_event('pulls', { current: 0 })
+  send_event('films', { current: 42 })
+  send_event('tv', { current: 42 })
+  send_event('podcasts', { current: 42 })
+  send_event('videos', { current: 42 })
+  send_event('pocket', { current: 42 })
+  send_event('books', { current: 42 })
+  send_event('rss', { current: 42 })
+  send_event('newsletter', { current: 42 })
+  send_event('blog', { current: 42 })
+  send_event('kindle', { current: 42 })
+  send_event('pulls', { current: 42 })
   send_event('forks', { items: [{"label" => "TODO", "value" => "TODO"}] })
 end
