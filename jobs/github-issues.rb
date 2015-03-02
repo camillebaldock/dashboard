7c7fcc5f5695e0a611308df66c29c4ef52281de7
Camille Baldock <camille@camillebaldock.co.uk>
2015-03-02 21:52:12 +0000

    Ensure issue unicity

--- jobs/github-issues.rb
+++ jobs/github-issues.rb
@@ -6,9 +6,10 @@ SCHEDULER.every "30m" do
   begin
     Octokit.auto_paginate = true
     client = Octokit::Client.new(:access_token => ENV["GITHUB_TOKEN"])
-    assigned = client.user_issues(:state => "open", :filter => "assigned")
-    created = client.user_issues(:state => "open", :filter => "created")
-    send_event("github-issues", { current: assigned.length + created.length })
+    assigned_ids = client.user_issues(:state => "open", :filter => "assigned").map(&:id)
+    created_ids = client.user_issues(:state => "open", :filter => "created").map(&:id)
+    all_issue_ids = (assigned_ids + created_ids).uniq
+    send_event("github-issues", { current: all_issue_ids.count })
   rescue Exception => e
     logger.exception(e)
   end
