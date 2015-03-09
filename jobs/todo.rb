c7530538a5d5e94a4f39753a9d26f1b0e56bfa24
Camille Baldock <camille@camillebaldock.co.uk>
2015-03-09 23:19:50 +0000

    Plex videos

--- jobs/todo.rb
+++ jobs/todo.rb
@@ -1,5 +1,4 @@
 SCHEDULER.every "1h" do
-  send_event('videos', { current: 42 })
   send_event('pocket', { current: 42 })
   send_event('books', { current: 42 })
   send_event('kindle', { current: 42 })
