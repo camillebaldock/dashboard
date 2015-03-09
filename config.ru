55f61e9d756e18340618190f9ea7d1e5412b1bda
Camille Baldock <camille@camillebaldock.co.uk>
2015-03-09 12:23:08 +0000

    Films and TV scripts

--- config.ru
+++ config.ru
@@ -1,12 +1,12 @@
 require 'dashing'
 require 'redis-objects'
 
-#redis_uri = URI.parse(ENV["REDISTOGO_URL"])
-#Redis.current = Redis.new(:host => redis_uri.host,
-#                          :port => redis_uri.port,
-#                          :password => redis_uri.password)
+redis_uri = URI.parse(ENV["REDISTOGO_URL"])
+Redis.current = Redis.new(:host => redis_uri.host,
+                          :port => redis_uri.port,
+                          :password => redis_uri.password)
 
-#set :history, Redis::HashKey.new('dashing-hash')
+set :history, Redis::HashKey.new('dashing-hash')
 
 configure do
   set :auth_token, ENV["AUTH_TOKEN"]
---
+++ scripts/plex.rb
@@ -0,0 +1,28 @@
+require 'plex-ruby'
+require 'json'
+
+server = Plex::Server.new(ENV["SERVER_HOST"], 32400)
+sections = server.library.sections
+tv_shows_section = sections.find{|section| section.title == "TV Shows" }
+tv_shows = tv_shows_section.all
+unwatched_tv_shows = 0
+tv_shows.each do |tv_show|
+  unwatched_tv_shows += tv_show.leaf_count.to_i - tv_show.viewed_leaf_count.to_i
+end
+films_section = sections.find{|section| section.title == "Movies" }
+unwatched_films = films_section.unwatched.count
+
+auth_token=ENV["AUTH_TOKEN"]
+
+json_headers = {"Content-Type" => "application/json",
+                "Accept" => "application/json"}
+
+params = {'auth_token' => auth_token, 'current' => unwatched_tv_shows}
+uri = URI.parse('http://dashboard.camillebaldock.com/widgets/tv')
+http = Net::HTTP.new(uri.host, uri.port)
+response = http.post(uri.path, params.to_json, json_headers)
+
+params = {'auth_token' => auth_token, 'current' => unwatched_films}
+uri = URI.parse('http://dashboard.camillebaldock.com/widgets/films')
+http = Net::HTTP.new(uri.host, uri.port)
+response = http.post(uri.path, params.to_json, json_headers)
