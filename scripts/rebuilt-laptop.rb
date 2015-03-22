2ad4c384fd68dede2c86fce824eefe3e4e3a9e45
Camille Baldock <camille@camillebaldock.co.uk>
2015-03-22 22:37:36 +0000

    Laptop rebuild

---
+++ scripts/rebuilt-laptop.rb
@@ -0,0 +1,14 @@
+require 'uri'
+require 'net/http'
+require 'json'
+
+auth_token=ENV["AUTH_TOKEN"]
+last_rebuild='14 February 2015'
+
+json_headers = {"Content-Type" => "application/json",
+                "Accept" => "application/json"}
+
+params = {'auth_token' => auth_token, 'since_date' => last_rebuild, 'red_after' => 30}
+uri = URI.parse('http://dashboard.camillebaldock.com/widgets/laptop')
+http = Net::HTTP.new(uri.host, uri.port)
+response = http.post(uri.path, params.to_json, json_headers)
