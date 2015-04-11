2de1d59d5a09ab99a571db8cf33e296432ce4297
Camille Baldock <camille@camillebaldock.co.uk>
2015-04-11 15:08:29 +0100

    Fix error in random aww script

--- jobs/randomaww.rb
+++ jobs/randomaww.rb
@@ -18,10 +18,10 @@ SCHEDULER.every '2m', first_in: 0 do |job|
       # Ensure we're linking directly to an image, not a gallery etc.
       valid_urls = urls.select{|url| url.downcase.end_with?('png', 'gif', 'jpg', 'jpeg')}
       random_awws = valid_urls.sample(4)
-      send_event('aww-1', image: random_aww[0])
-      send_event('aww-2', image: random_aww[1])
-      send_event('aww-3', image: random_aww[2])
-      send_event('aww-4', image: random_aww[3])
+      send_event('aww-1', image: random_awws[0])
+      send_event('aww-2', image: random_awws[1])
+      send_event('aww-3', image: random_awws[2])
+      send_event('aww-4', image: random_awws[3])
     end
   rescue Exception => e
     logger.exception(e)
