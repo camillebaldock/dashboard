4c700027f4762c1fd5d8d2472bb3012a57d76f17
Camille Baldock <camille@camillebaldock.co.uk>
2015-04-11 02:54:59 +0100

    Home page redesign

--- jobs/randomaww.rb
+++ jobs/randomaww.rb
@@ -17,8 +17,11 @@ SCHEDULER.every '2m', first_in: 0 do |job|
       urls = json['data']['children'].map{|child| child['data']['url'] }
       # Ensure we're linking directly to an image, not a gallery etc.
       valid_urls = urls.select{|url| url.downcase.end_with?('png', 'gif', 'jpg', 'jpeg')}
-      random_aww = valid_urls.sample(1).first
-      send_event('aww', image: random_aww)
+      random_awws = valid_urls.sample(4)
+      send_event('aww-1', image: random_aww[0])
+      send_event('aww-2', image: random_aww[1])
+      send_event('aww-3', image: random_aww[2])
+      send_event('aww-4', image: random_aww[3])
     end
   rescue Exception => e
     logger.exception(e)
