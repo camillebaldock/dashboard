3f7355770ea7c60765419345a0aaa5f7b9b10d71
Camille Baldock <camille@camillebaldock.co.uk>
2015-04-11 19:19:59 +0100

    Travis

--- jobs/travis.rb
+++ jobs/travis.rb
@@ -1,8 +1,27 @@
-SCHEDULER.every "30m", :first_in => 0 do
+require 'net/https'
+require 'json'
+
+SCHEDULER.every "10m", :first_in => 0 do
   logger = Logger.new("travis")
   logger.start
+  broken_builds = 0
   begin
-    send_event('travis', { current: 0 })
+    http = Net::HTTP.new("api.travis-ci.org", 443)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    repos = ENV["TRAVIS_REPOS"].split(",")
+    repos.each do |repo|
+      response = http.request(Net::HTTP::Get.new("/repositories/#{ENV["GITHUB_USER"]}/#{repo}.json"))
+      usage = JSON.parse(response.body)
+      if usage["last_build_status"].to_i != 0
+        broken_builds += 1
+      end
+    end
+    if broken_builds > 0
+      send_event('travis', { current: broken_builds, status: 'danger' })
+    else
+      send_event('travis', { status: 'ok' })
+    end
   rescue Exception => e
     logger.exception(e)
   end
