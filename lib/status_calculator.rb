b141dbc0e8745c9ea9656f52b490dd7fdf77843e
Camille Baldock <camille@camillebaldock.co.uk>
2015-06-08 22:39:01 +0100

    Status calculator: finish implementation

--- lib/status_calculator.rb
+++ lib/status_calculator.rb
@@ -2,10 +2,17 @@ class StatusCalculator
 
   def initialize(settings)
     @settings = settings
+    @limits = settings.values.sort
   end
 
   def run(number)
-    "ok"
+    lower_limits = @limits.select{ |limit| limit <= number }
+    if lower_limits.empty?
+      "ok"
+    else
+      largest_lower_limit = lower_limits.max
+      @settings.key(largest_lower_limit)
+    end
   end
 
 end
--- spec/status_calculator_spec.rb
+++ spec/status_calculator_spec.rb
@@ -3,37 +3,40 @@ require 'status_calculator'
 
 describe StatusCalculator do
 
-  let(:settings) { {} }
+  let(:settings) {
+    {
+      "attention" => attention,
+      "danger" => danger,
+      "warning" => warning
+    }
+  }
+  let(:attention) { 5 }
+  let(:danger) { 20 }
+  let(:warning) { 50 }
   let(:number) { Random.rand(100) }
   let(:result) { described_class.new(settings).run(number) }
 
   context "its settings are an attention, a danger and a warning value" do
     context "number is < to attention" do
+      let(:number) { rand(0..attention-1) }
       it "returns ok" do
         expect(result).to eq "ok"
       end
     end
     context "number is >= to attention and < danger" do
+      let(:number) { rand(attention..danger-1) }
       it "returns attention" do
         expect(result).to eq "attention"
       end
     end
-    context "number is == danger" do
-      it "returns danger" do
-        expect(result).to eq "danger"
-      end
-    end
     context "number is >= danger and < warning" do
+      let(:number) { rand(danger..warning-1) }
       it "returns danger" do
         expect(result).to eq "danger"
       end
     end
-    context "number is == to warning" do
-      it "returns warning" do
-        expect(result).to eq "warning"
-      end
-    end
-    context "number is > to warning" do
+    context "number is >= to warning" do
+      let(:number) { rand(warning..warning+1000) }
       it "returns warning" do
         expect(result).to eq "warning"
       end
@@ -41,32 +44,21 @@ describe StatusCalculator do
   end
 
   context "its settings are only a danger value" do
+    let(:settings) {
+      {
+        "danger" => danger
+      }
+    }
     context "number is < to danger" do
+      let(:number) { rand(0..danger-1) }
       it "returns ok" do
         expect(result).to eq "ok"
       end
     end
     context "number is >= to danger" do
+      let(:number) { rand(danger..danger+1000) }
       it "returns danger" do
         expect(result).to eq "danger"
-      end
-    end
-  end
-
-  context "its settings are an attention and a warning value" do
-    context "number is < to attention" do
-      it "returns ok" do
-        expect(result).to eq "ok"
-      end
-    end
-    context "number is >= to attention and < to warning" do
-      it "returns attention" do
-        expect(result).to eq "attention"
-      end
-    end
-    context "number is >= to warning" do
-      it "returns warning" do
-        expect(result).to eq "warning"
       end
     end
   end
