a994d52d98e080199deabd6ba43084c5555e318c
Camille Baldock <camille@camillebaldock.co.uk>
2015-08-03 23:10:21 +0100

    client missing

--- jobs/github-forks.rb
+++ jobs/github-forks.rb
@@ -1,6 +1,6 @@
 require 'octokit'
 
-def is_in_sync(parent, fork)
+def is_in_sync(client, parent, fork)
   parent_commits = parent.rels[:commits].get.data
   latest_parent_commit_date = parent_commits.first.commit.committer.date
   latest_parent_commit_sha = parent_commits.first.sha
@@ -28,7 +28,7 @@ SCHEDULER.every "30m", first_in: 0 do
     forks.each do |fork|
       if fork.full_name.include?(ENV["GITHUB_USER"])
         parent = client.repository("#{ENV["GITHUB_USER"]}/#{fork.name}").parent
-        unless is_in_sync(parent, fork)
+        unless is_in_sync(client, parent, fork)
           out_of_date_forks << fork.name
         end
       end
@@ -38,7 +38,7 @@ SCHEDULER.every "30m", first_in: 0 do
       repos = manual_fork.split(",")
       parent = client.repository(repos[0])
       fork = client.repository(repos[1])
-      unless is_in_sync(parent, fork)
+      unless is_in_sync(client, parent, fork)
         out_of_date_forks << fork.name
       end
     end
