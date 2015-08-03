6199b8c95b97aef531d9d4124e8d7d586f56af62
Camille Baldock <camille@camillebaldock.co.uk>
2015-08-03 23:18:41 +0100

    background color delays

--- jobs/updates.rb
+++ jobs/updates.rb
@@ -3,9 +3,9 @@ SCHEDULER.every "1h", :first_in => 0 do
   logger.start
   begin
     update_document = YAML.load(File.open("./data/updates.yml"))
-    send_event("laptop", { since_date: update_document["laptop"] } )
-    send_event("blog", { since_date: update_document["blog"] } )
-    send_event("newsletter", { since_date: update_document["newsletter"] } )
+    send_event("laptop", { since_date: update_document["laptop"], red_after: 60*3600*24 } )
+    send_event("blog", { since_date: update_document["blog"], red_after: 30*3600*24 } )
+    send_event("newsletter", { since_date: update_document["newsletter"], red_after: 30*3600*24 } )
   rescue Exception => e
     logger.exception(e)
   end
