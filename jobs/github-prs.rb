9a18aa28927d714647e41fb195fdcabf9090d550
Camille Baldock <camille@camillebaldock.co.uk>
2015-08-04 09:11:07 +0100

    Monitor GitHub orgs as part of pull request count too

--- jobs/github-prs.rb
+++ jobs/github-prs.rb
@@ -7,15 +7,21 @@ SCHEDULER.every "30m", first_in: 0 do
     Octokit.auto_paginate = true
     client = Octokit::Client.new(:access_token => ENV["GITHUB_TOKEN"])
     number_prs = 0
+
+    ENV["MONITORED_GITHUB_ORGS"].split(",").each do |monitored_github_org|
+      org_repos = client.organization_repositories(monitored_github_org).map { |repo| repo.name }
+      org_repos.each do |org_repo|
+        prs = client.pull_requests("#{monitored_github_org}/#{org_repo}")
+        number_prs += prs.count
+      end
+    end
+
     repos = client.repositories(:user => ENV["GITHUB_USER"])
     user_repos = repos.select do |repo|
       repo.owner.login == ENV["GITHUB_USER"]
     end
     user_repos.each do |user_repo|
       prs = client.pull_requests(ENV["GITHUB_USER"]+"/"+user_repo.name, :state => 'open')
-      prs.each do |pr|
-        logger.info(pr.title)
-      end
       number_prs += prs.count
     end
     settings = {
