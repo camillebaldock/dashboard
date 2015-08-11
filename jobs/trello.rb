861ac3d902480d93ab711bf2e10d685411fa3820
Camille Baldock <camille@camillebaldock.co.uk>
2015-08-12 00:59:05 +0100

    Training and challenges

--- jobs/trello.rb
+++ jobs/trello.rb
@@ -17,6 +17,16 @@ SCHEDULER.every "30m", first_in: 0 do
       list = lists.find { |list| list.name == list_name }
       send_event("trello-#{list_name.downcase}", { current: list.cards.size })
     end
+    # Training
+    training = Board.all.find { |board| board.name == "Training" }
+    training_lists = training.lists
+    ongoing_training_cards = training_lists.select { |list| list.name != "Done" }.map(&:cards).map(&:size).inject(&:+)
+    send_event("training", { current: ongoing_training_cards })
+    # Challenges
+    challenges = Board.all.find { |board| board.name == "Challenges" }
+    challenge_lists = challenges.lists
+    ongoing_challenge_cards = challenge_lists.select { |list| list.name != "Done" }.map(&:cards).map(&:size).inject(&:+)
+    send_event("challenges", { current: ongoing_challenge_cards })
   rescue Exception => e
     logger.exception(e)
   end
