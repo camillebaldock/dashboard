949225363a099cb8f8bc0669558b66ad2e47a0da
Camille Baldock <camille@camillebaldock.co.uk>
2015-08-15 12:23:40 +0100

    Time since last

--- jobs/updates.rb
+++ jobs/updates.rb
@@ -1,11 +1,69 @@
+require 'date'
+
+def calculate_color(number_string, settings)
+  status_calculator = StatusCalculator.new(settings)
+  status_calculator.get_color(number_string.to_i)
+end
+
+def days_ago(date_string)
+  number_days = (Date.today - Date.parse(date_string)).to_i
+  if number_days < 1
+    "less than 1 day ago"
+  elsif number_days == 1
+    "1 day ago"
+  elsif number_days <30
+    "#{number_days} days ago"
+  elsif number_days < 365
+    "#{number_days/30} months ago"
+  else
+    "more than 1 year ago"
+  end
+end
+
+def number_days(date_string)
+  (Date.today - Date.parse(date_string)).to_i
+end
+
 SCHEDULER.every "1h", :first_in => 0 do
   logger = Logger.new("updates")
   logger.start
   begin
     update_document = YAML.load(File.open("./data/updates.yml"))
-    send_event("laptop", { since_date: update_document["laptop"], red_after: 60*3600*24 } )
-    send_event("blog", { since_date: update_document["blog"], red_after: 30*3600*24 } )
-    send_event("newsletter", { since_date: update_document["newsletter"], red_after: 30*3600*24 } )
+    laptop_settings = {
+      "danger" => 60,
+      "warning" => 90
+    }
+    blog_settings = {
+      "danger" => 30,
+      "warning" => 60
+    }
+    newsletter_settings = {
+      "danger" => 30,
+      "warning" => 60
+    }
+    formatted_updates = {}
+    update_document.each do |update_entry, update_value|
+      formatted_updates[update_entry] = {
+        "ago" => days_ago(update_value),
+        "number_days" => number_days(update_value)
+      }
+    end
+    p formatted_updates
+    send_event("laptop", {
+        "text" => formatted_updates["laptop"]["ago"],
+        "background-color" => calculate_color(formatted_updates["laptop"]["number_days"], laptop_settings)
+      }
+    )
+    send_event("blog", {
+        "text" => formatted_updates["blog"]["ago"],
+        "background-color" => calculate_color(formatted_updates["blog"]["number_days"], blog_settings)
+      }
+    )
+    send_event("newsletter", {
+        "text" => formatted_updates["newsletter"]["ago"],
+        "background-color" => calculate_color(formatted_updates["newsletter"]["number_days"], newsletter_settings)
+      }
+    )
   rescue Exception => e
     logger.exception(e)
   end
--- widgets/time_since_last/time_since_last.coffee
+++ widgets/time_since_last/time_since_last.coffee
@@ -1,45 +1,5 @@
 class Dashing.TimeSinceLast extends Dashing.Widget
 
-  ready: ->
-    @last_event = moment(new Date(localStorage.getItem(@get('id')+'_last_event')))
-    @last_event = moment().format() unless @last_event
-    setInterval(@startTime, 500)
-
   onData: (data) ->
-    if(@get('since_date'))
-      localStorage.setItem(@get('id')+'_last_event', moment(new Date(@get('since_date'))).format())
-
-    @last_event = moment(new Date(localStorage.getItem(@get('id')+'_last_event')))
-    $(@node).fadeOut().css('background-color', @backgroundColor).fadeIn()
-
-  startTime: =>
-    delta = moment().unix() - moment(@last_event).unix()
-    t = Math.abs(delta)
-
-    hour = 60 * 60
-    m = Math.floor(t / 60)
-    s = Math.floor(t % 60)
-
-    if t > hour
-      @set('time_past', moment(@last_event).fromNow())
-    else if delta > 0
-      @set('time_past', "+ " + @formatTime(m) + ":" + @formatTime(s))
-    else
-      @set('time_past', "- " + @formatTime(m) + ":" + @formatTime(s))
-
-    @set('moreinfo', moment(@last_event).format('MMMM Do YYYY HH:mm a'))
-
-  formatTime: (i) ->
-    if i < 10 then "0" + i else i
-
-  backgroundColor: =>
-    if (@get('red_after'))
-      redAfter = parseInt(@get('red_after'))
-    else
-      redAfter = 100
-
-    diff = moment().unix() - moment(@last_event).unix()
-    if (diff > redAfter)
-      "red"
-    else
-      "green"
+    if data['background-color']
+      $(@get('node')).css 'background-color', data['background-color']
